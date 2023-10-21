145296ce332c3a1784bbfa6ec0c9363abef02cd4
Anm11411422ubca8667401193 <145526857+Anm11411422ubca8667401193@users.noreply.github.com>
2023-10-21 23:05:31 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,22 +1,24 @@
-class Account:
-    def __init__(self):
-        self.balance=0
-        print('Your Account is Created.')
-    def deposit(self):
-        amount=int(input('Enter the amount to deposit:'))
-        self.balance+=amount
-        print('Your New Balance =%d' %self.balance)
-    def withdraw(self):
-        amount=int(input('Enter the amount to withdraw:'))
-        if(amount>self.balance):
-            print('Insufficient Balance!')
-        else:
-            self.balance-=amount
-        print('Your Remaining Balance =%d' %self.balance)
-    def enquiry(self):
-        print('Your Balance =%d' %self.balance)
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-account= Account()
-account.deposit()
-account.withdraw()
-account.enquiry()
+
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
