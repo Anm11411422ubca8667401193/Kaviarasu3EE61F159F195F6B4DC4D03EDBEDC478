fa22b81dc565a9715280f1942339a043005a4b05
Anm11411422ubca8667401193 <145526857+Anm11411422ubca8667401193@users.noreply.github.com>
2023-10-21 22:35:04 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,24 +1,22 @@
-#1.1 Implement o recursive function tocalculate the factorial of a given number
+class Account:
+    def __init__(self):
+        self.balance=0
+        print('Your Account is Created.')
+    def deposit(self):
+        amount=int(input('Enter the amount to deposit:'))
+        self.balance+=amount
+        print('Your New Balance =%d' %self.balance)
+    def withdraw(self):
+        amount=int(input('Enter the amount to withdraw:'))
+        if(amount>self.balance):
+            print('Insufficient Balance!')
+        else:
+            self.balance-=amount
+        print('Your Remaining Balance =%d' %self.balance)
+    def enquiry(self):
+        print('Your Balance =%d' %self.balance)
 
-"""
-1!=1×1
-2!=2 x 1! ---> 2 x 1 
-3!= 3 ×2! --->3 x 1
-.
-.
-10!=10 x 9! ---> 10 x 9 x... x 1
-
-Formula - n ×(n-1)!
-"""
-
-
-def fact_rec(n):
-      if n==0 or n==1:
-         return 1 
-      else:
-         return n*fact_rec(n-1)
-
-number = int(input("Enter a value :"))
-res = fact_rec (number)
-
-print("The factorial of {} is {}.".format(number,res))
+account= Account()
+account.deposit()
+account.withdraw()
+account.enquiry()
